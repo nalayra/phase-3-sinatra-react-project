4e441db5846531027c59c4626968cae8ddc6f348
Nicholas Alayra <nalayra@hotmail.com>
2022-01-25 23:43:51 -0800

    fixed server

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,5 @@
+require "pry"
+
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
@@ -16,8 +18,20 @@ class ApplicationController < Sinatra::Base
     foods.to_json
   end
 
-  get '/foods/:category' do
-    foods = Food.select {|x| x.category == "#{params[:category]}"}
+  get '/foods/by_category/:category' do
+    foods = Food.select{|x| x.category == params[:category]}
+    # foods = Food.where(category: params[:category])
+    foods.to_json
+  end
+
+  get '/foods/by_name/:name' do
+    foods = Food.select {|x| x.name == params[:name]}
+    # foods = Food.all.find_by(name: params[:name])
+    foods.to_json
+  end
+
+  get '/foods/by_calories/:calories' do
+    foods = Food.all.select{|x| x.calories == params[:calories].to_i}
     foods.to_json
   end
 
